699537e04629a3db1a7ebd001ace462b3b3e0892
xiaoyao-Ye <yqcyecong@163.com>
2023-02-12 03:54:12 +0800

    feat: 🎸 unocss

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
+import UnoCSS from 'unocss/vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'node:path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    UnoCSS({
+      /* options */
+    }),
+  ],
+  resolve: {
+    alias: { '@/': `${path.resolve(__dirname, 'src')}/` },
+  },
 })
