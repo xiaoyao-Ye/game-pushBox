5efc4ec7b12a2a29e80c4de6bbbc96f4732784e6
xiaoyao-Ye <yqcyecong@163.com>
2023-02-12 19:34:17 +0800

    feat: 🎸 complete the game logic

--- src/views/index.vue
+++ src/views/index.vue
@@ -2,25 +2,28 @@
   <div class="h-screen flex justify-center items-center">
     <div>
       <div class="flex" v-for="(row, rowIndex) in map" :key="rowIndex">
-        <div class="w-14 h-14" :class="[mapBackGround[col]]" v-for="(col, colIndex) in row" :key="colIndex"></div>
+        <div class="w-14 h-14" :class="[mapBackGround[col]]" v-for="(col, colIndex) in row" :key="colIndex">
+          {{ rowIndex + '-' + colIndex }}
+        </div>
       </div>
     </div>
     <div class="text-6">
       <div>第{{ level }}关</div>
-      <div>重新kaishi</div>
-      <div>上一关</div>
-      <div>下一关</div>
+      <div @click="initGame">重新kaishi</div>
+      <div @click="onPreLevel">上一关</div>
+      <div @click="onNextLevel">下一关</div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import { maps } from '@/utils/maps'
-import { ref } from 'vue'
+import { nextTick, onMounted, ref } from 'vue'
 import { onKeyUp } from '@vueuse/core'
 
 const level = ref<number>(1)
 const personLocation = ref<number[]>([])
+const boxTargetLocation = ref<number[][]>([])
 const map = ref<number[][]>([])
 const mapBackGround = ref<{ [key: number]: any }>({
   0: 'bg-white', // road 地面
@@ -30,19 +33,38 @@ const mapBackGround = ref<{ [key: number]: any }>({
   5: 'bg-black', // box 目标位置
 })
 
+const onPreLevel = () => {
+  if (level.value <= 1) return
+  level.value -= 1
+  initGame()
+}
+const onNextLevel = () => {
+  if (level.value >= 42) return
+  level.value += 1
+  initGame()
+}
+
 const initGame = () => {
-  map.value = maps.slice(level.value - 1, 1)[0]
+  console.log(maps)
+  console.log(level.value)
+  boxTargetLocation.value.length = 0
+  map.value = JSON.parse(JSON.stringify(maps.slice(level.value - 1, level.value)[0]))
+  console.log('map', map.value)
   map.value.forEach((row, rowIndex) => {
+    // 记录人的位置
     if (row.includes(2)) {
       const colIndex = row.findIndex((item) => item === 2)
       personLocation.value = [rowIndex, colIndex]
     }
+    // 记录箱子的终点位置
+    if (row.includes(5)) {
+      row.forEach((col, colIndex) => {
+        if (col === 5) {
+          boxTargetLocation.value.push([rowIndex, colIndex])
+        }
+      })
+    }
   })
-
-  setKeyboardEvents('ArrowDown')
-  setKeyboardEvents('ArrowUp')
-  setKeyboardEvents('ArrowLeft')
-  setKeyboardEvents('ArrowRight')
 }
 
 type Direction = 'ArrowDown' | 'ArrowUp' | 'ArrowLeft' | 'ArrowRight'
@@ -56,20 +78,39 @@ const setKeyboardEvents = (key: Direction) => {
 
 const move = (direction: Direction) => {
   // 计算位置
-  const targetLocation = getTargetLocation(direction)
   const [rowIndex, colIndex] = personLocation.value
+  const targetLocation = getTargetLocation(direction, rowIndex, colIndex)
   const [targetRowIndex, targetColIndex] = targetLocation
 
-  // TODO: 判断是否可以移动
-  if (map.value[targetRowIndex][targetColIndex])
-    // 移动
-    map.value[rowIndex][colIndex] = 0
+  // 判断是否可以移动
+  const targetValue = map.value[targetRowIndex][targetColIndex]
+  const [nextTargetRowIndex, nextTargetColIndex] = getTargetLocation(direction, targetRowIndex, targetColIndex)
+  const nextTargetValue = map.value[nextTargetRowIndex][nextTargetColIndex]
+
+  // 碰到墙壁无法移动
+  const hitWall = targetValue === 1
+  // 推到箱子, 并且箱子后面不是地板, 推不动
+  const isNotFloor = targetValue === 4 && nextTargetValue !== 0 && nextTargetValue !== 5
+  if (hitWall || isNotFloor) return
+
+  // 移动人
+  map.value[rowIndex][colIndex] = 0
   map.value[targetRowIndex][targetColIndex] = 2
   personLocation.value = [targetRowIndex, targetColIndex]
+
+  // 移动箱子(可以移动, 并且推到箱子)
+  if (targetValue === 4) {
+    map.value[nextTargetRowIndex][nextTargetColIndex] = 4
+    // 箱子有移动, 判断是否过关
+    const isPass = boxTargetLocation.value.every(([rowIndex, colIndex]) => map.value[rowIndex][colIndex] === 4)
+    if (isPass) {
+      level.value += 1
+      initGame()
+    }
+  }
 }
 
-const getTargetLocation = (direction: Direction) => {
-  const [rowIndex, colIndex] = personLocation.value
+const getTargetLocation = (direction: Direction, rowIndex: number, colIndex: number) => {
   let targetLocation: number[] = []
   switch (direction) {
     case 'ArrowDown':
@@ -87,13 +128,16 @@ const getTargetLocation = (direction: Direction) => {
   }
   return targetLocation
 }
-// 封装逻辑复用
 // 利用计算属性(是否过关? 移动?)
-// person 只在移动碰到箱子时, 调用通关检测
-// person 前面不是墙壁 || 前面的箱子的前面不是墙壁, 都可行走
 // person 从箱子终点移出时, 需要复原箱子终点颜色!?
 
-initGame()
+onMounted(() => {
+  initGame()
+  setKeyboardEvents('ArrowDown')
+  setKeyboardEvents('ArrowUp')
+  setKeyboardEvents('ArrowLeft')
+  setKeyboardEvents('ArrowRight')
+})
 </script>
 
 <style scoped></style>
