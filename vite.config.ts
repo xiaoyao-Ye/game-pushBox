7358c73b66e4a81c4b8d901bffdd5482bba117f0
xiaoyao-Ye <yqcyecong@163.com>
2023-02-16 23:08:49 +0800

    feat: 🎸 add base

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import AutoImport from 'unplugin-auto-import/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/game-pushBox/',
   plugins: [
     vue(),
     UnoCSS({}),
